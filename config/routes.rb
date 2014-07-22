cb3833e33843906d032e243f01dc4cbc7dab6a8f
Syswow128 <syswow128@gmail.com>
2014-07-22 14:23:03 +0400

    User session

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  devise_for :users
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
 end
