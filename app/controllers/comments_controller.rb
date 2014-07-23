4ec61aed62a872354175dd3329ebe614b119bb5e
aufarik <zaufar@gmail.com>
2014-07-23 13:30:07 +0400

    Added author controls(Edit, Destroy) only for authors. For both comments and articles.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+
     redirect_to article_path(@article)
   end
   
